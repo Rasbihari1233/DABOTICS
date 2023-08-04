eceb48170a2f2ef9d836a4c06f8b2b23d6f35f3f
Ras Bihari <rasuthakur1233@gmail.com>
2023-08-04 14:34:56 +0530

    Task towo added

---
+++ Task2/Script.js
@@ -0,0 +1,33 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+let value = document.querySelectorAll('.button').values;
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        try {
+            if (e.target.innerHTML == '=') {
+                string = eval(string);
+                document.querySelector('input').value = string;
+            }
+            else if (e.target.innerHTML == 'AC') {
+                string = "";
+                document.querySelector('input').value = string;
+            }
+            else if (e.target.innerHTML == '√') {
+                string = Math.sqrt(string);
+                document.querySelector('input').value = string;
+            }
+            else if (e.target.innerHTML == 'C') {
+                string = string.slice(0, string.length - 1);
+                document.querySelector('input').value = string;
+            }
+            else {
+                console.log(e.target)
+                string = string + e.target.innerHTML;
+                document.querySelector('input').value = string;
+            }
+        } catch {
+            document.querySelector('input').value = 'SYNTAX ERROR';
+        }
+    })
+})
